a563499125abb005505aa9977c8d516021e0261c
vermaji <rajaditya14032002@gmail.com>
2022-06-22 12:11:24 +0530

    creating routes and schema

---
+++ Backend/routes/auth.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let router = express.Router();
+
+router.get("/", (req, res) => {
+  obj = {
+    a: "thios",
+    number: "34",
+  };
+  res.json(obj);
+});
+
+module.exports = router;
